83ad6d9aef5e61ae366126330e49ba0a973be437
deebot10 <dee1jhill@gmail.com>
2021-06-03 20:25:02 -0600

    Red for Merchant story 2 test

--- spec/features/merchant/index_spec.rb
+++ spec/features/merchant/index_spec.rb
@@ -5,11 +5,24 @@ RSpec.describe 'merchants' do
     @merchant = Merchant.create!(name: 'H&M')
   end
 
-  # Merchant Story 1 
-  it 'visits the Merchant Dashboard' do
-    visit merchant_dashboard_index_path(@merchant)
+  describe 'Merchant Dash' do
+    # Merchant Story 1 
+    it 'visits the Merchant Dashboard' do
+      visit merchant_dashboard_index_path(@merchant)
+      
+      expect(page).to have_content('Merchant Dashboard')
+      expect(page).to have_content(@merchant.name)
+    end
     
-    expect(page).to have_content('Merchant Dashboard')
-    expect(page).to have_content(@merchant.name)
+    # As a merchant,
+    # When I visit my merchant dashboard
+    # Then I see link to my merchant items index (/merchants/merchant_id/items)
+    # And I see a link to my merchant invoices index (/merchants/merchant_id/invoices)
+    it 'has links on the Merchant Dashboard' do  
+      visit merchant_dashboard_index_path(@merchant)
+      
+      expect(page).to have_content('Items')
+      expect(page).to have_link(merchant_items_path)
+    end
   end
 end
