ea051f201c71ff3c12d0e3b3382dd5610a3bb36b
Emmy Morris <emmymorris1102@gmail.com>
2021-06-05 12:30:57 -0600

    Pass Admin Merchant update story

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -11,6 +11,16 @@ class Admin::MerchantsController < ApplicationController
     @merchant = Merchant.find(params[:id])
   end
 
+  def update
+    @merchant = Merchant.find(params[:id])
+    @merchant.update(merchant_params)
+    if @merchant.save
+      redirect_to "/admin/merchants/#{@merchant.id}",
+      error: "Merchant has been successfully updated"
+      # flash[:success] = "Merchant has been successfully updated"
+    end
+  end
+
   private
   def merchant_params
     params.permit(:name)
--- spec/features/admin/merchants/show_spec.rb
+++ spec/features/admin/merchants/show_spec.rb
@@ -7,6 +7,17 @@ RSpec.describe 'Admin Merchant' do
     # When I click on the name of a merchant from the admin merchants index page,
     # Then I am taken to that merchant's admin show page (/admin/merchants/merchant_id)
     # And I see the name of that merchant
+    # Admin Merchant Update
+
+    # As an admin,
+    # When I visit a merchant's admin show page
+    # Then I see a link to update the merchant's information.
+    # When I click the link
+    # Then I am taken to a page to edit this merchant
+    # And I see a form filled in with the existing merchant attribute information
+    # When I update the information in the form and I click ‘submit’
+    # Then I am redirected back to the merchant's admin show page where I see the updated information
+    # And I see a flash message stating that the information has been successfully updated.
     @merchant = Merchant.create!(name: 'H&M')
 
     visit "/admin/merchants/#{@merchant.id}"
@@ -16,5 +27,10 @@ RSpec.describe 'Admin Merchant' do
     expect(page).to have_link("Update #{@merchant.name}")
     click_link "Update #{@merchant.name}"
     expect(current_path).to eq("/admin/merchants/#{@merchant.id}/edit")
+    fill_in 'Name', with: 'Claires'
+    click_button "Update #{@merchant.name}"
+    expect(current_path).to eq("/admin/merchants/#{@merchant.id}")
+    save_and_open_page
+    expect(page).to have_content("Merchant has been successfully updated")
   end
 end
