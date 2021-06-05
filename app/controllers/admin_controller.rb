a8366640657d20ba0720206e780dd96121631ebc
Alex Ferencz <alex.ferencz6@gmail.com>
2021-06-04 21:54:05 -0600

    Start admin merchants

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,5 +2,6 @@ class AdminController < ApplicationController
   def index
     @top_5 = Customer.successful_transactions
     @incomplete_invoices = Invoice.incomplete_invoices_sorted_by_date
+    
   end
 end
