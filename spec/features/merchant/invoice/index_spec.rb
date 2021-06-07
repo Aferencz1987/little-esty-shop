ee1a3492a12ee3b8dde2d89481ea3814c7007011
deebot10 <dee1jhill@gmail.com>
2021-06-07 12:14:24 -0600

    Creted set up for Merchant index spec

--- spec/features/merchant/invoice/index_spec.rb
+++ spec/features/merchant/invoice/index_spec.rb
@@ -3,6 +3,23 @@ require 'rails_helper'
 RSpec.describe 'Merchant Invoice' do
   before(:each) do
     @merchant = Merchant.create!(name: 'H&M')  
+    @merchant_2 = Merchant.create!(name: 'Hot Topic')
+    @customer = Customer.create!(first_name: "Dee", last_name: "Hill")
+
+    @item_1 = Item.create!(name: 'Shoes', description: 'For your feet', unit_price: 10.0, merchant_id: @merchant.id)
+    @item_2 = Item.create!(name: 'Dress', description: 'Beautiful gown', unit_price: 12.4, merchant_id: @merchant.id)
+    @item_3 = Item.create!(name: 'Shorts', description: 'For basketball', unit_price: 11.2, merchant_id: @merchant_2.id)
+    @item_4 = Item.create!(name: 'Dress', description: 'Beautiful gown', unit_price: 12.4, merchant_id: @merchant_2.id)
+    
+    @invoice_1 = Invoices.create!(status: 1, merchant_id: @customer.id)
+    @invoice_2 = Invoices.create!(status: 1, merchant_id: @customer.id)
+    @invoice_3 = Invoices.create!(status: 1, merchant_id: @customer.id)
+    @invoice_4 = Invoices.create!(status: 1, merchant_id: @customer.id)
+    
+    @invoice_item_1 = InvoiceItem.create!(quantity: 2, unit_price: 14.9, status: 1, invoice_id: @invoice_1.id, item_id: @item_1.id) 
+    @invoice_item_2 = InvoiceItem.create!(quantity: 1, unit_price: 14.9, status: 1, invoice_id: @invoice_2.id, item_id: @item_2.id)
+    @invoice_item_3 = InvoiceItem.create!(quantity: 2, unit_price: 14.9, status: 1, invoice_id: @invoice_3.id, item_id: @item_3.id)
+    @invoice_item_4 = InvoiceItem.create!(quantity: 2, unit_price: 14.9, status: 1, invoice_id: @invoice_4.id, item_id: @item_4.id)
   end
 
     # As a merchant,
@@ -13,6 +30,7 @@ RSpec.describe 'Merchant Invoice' do
   it 'visits the Merchant invocie index' do
     visit merchant_invoices(@merchant)
 
-    expect(page).to have_content()
+    expect(page).to have_content('Merchant Invoices')
+    
   end    
 end
