c3f02dcd6afa1fe80ae1f0b08892212e54ebbfc9
Emmy Morris <77904287+EmmyMorris@users.noreply.github.com>
2021-06-09 09:50:38 -0600

    Update index_spec.rb

--- spec/features/admin/invoices/index_spec.rb
+++ spec/features/admin/invoices/index_spec.rb
@@ -11,10 +11,10 @@ RSpec.describe 'Admin Invoice' do
     @item_3 = Item.create!(name: 'Shorts', description: 'For basketball', unit_price: 11.2, merchant_id: @merchant_2.id)
     @item_4 = Item.create!(name: 'Dress', description: 'Beautiful gown', unit_price: 12.4, merchant_id: @merchant_2.id)
 
-    @invoice_1 = @customer.invoices.create!(status: 1, merchant_id: @merchant.id)
-    @invoice_2 = @customer.invoices.create!(status: 1, merchant_id: @merchant.id)
-    @invoice_3 = @customer.invoices.create!(status: 1, merchant_id: @merchant.id)
-    @invoice_4 = @customer.invoices.create!(status: 1, merchant_id: @merchant.id)
+    @invoice_1 = @customer.invoices.create!(status: 1)
+    @invoice_2 = @customer.invoices.create!(status: 1)
+    @invoice_3 = @customer.invoices.create!(status: 1)
+    @invoice_4 = @customer.invoices.create!(status: 1)
 
     @invoice_item_1 = InvoiceItem.create!(quantity: 2, unit_price: 14.9, status: 1, invoice_id: @invoice_1.id, item_id: @item_1.id)
     @invoice_item_2 = InvoiceItem.create!(quantity: 1, unit_price: 14.9, status: 1, invoice_id: @invoice_2.id, item_id: @item_2.id)
