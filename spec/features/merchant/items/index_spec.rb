4bc184f282effd69e5602155ae12a9460ae43b22
Carlyle Douglas <cdouglascorp@gmail.com>
2021-06-09 15:15:11 -0600

    Fix merchant file tree structure

--- spec/features/merchant/items/index_spec.rb
+++ spec/features/merchant/items/index_spec.rb
@@ -5,13 +5,14 @@
 # And I do not see items for any other merchant
 describe 'merchant item index' do
   it "displays a list of the names of the items" do
-    merchant = Merchant.create!(name: 'Ryan')
-    item1 = merchant.items.create!(name: 'Item1', description: 'Description Here', unit_price: 90)
-    item2 = merchant.items.create!(name: 'Item2', description: 'Description Here', unit_price: 90)
-    item3 = merchant.items.create!(name: 'Item3', description: 'Description Here', unit_price: 90)
+    @merchant = Merchant.create!(name: 'Ryan')
+    item1 = @merchant.items.create!(name: 'Item1', description: 'Description Here', unit_price: 90)
+    item2 = @merchant.items.create!(name: 'Item2', description: 'Description Here', unit_price: 90)
+    item3 = @merchant.items.create!(name: 'Item3', description: 'Description Here', unit_price: 90)
 
     # visit '/merchants/1/items'
-    visit merchant_items_path(merchant.id)
+    # visit merchant_items_path(merchant.id)
+    visit "/merchants/#{@merchant.id}/items"
 
     expect(page).to have_content(item1.name)
   end
